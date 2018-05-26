241addb5a4d556ebcc80c9b9ad3a18d944e9e711
Dean <mccarthey123@outlook.com>
2018-05-26 09:34:47 +0800

    *号表达式

--- destruct.py
+++ destruct.py
@@ -10,7 +10,35 @@ s = 'hello'
 x, y, _, _, _ = s
 print(x, y)
 
-# python星号表达式，类似于js中的运算展开符(...)
+# python星号表达式，类似于js中的运算展开符(...)，结构出来的变量是列表类型
 record = ('test', 24, '6', '8')
 name, age, *lucky_number = record
 print(name, age, lucky_number)
+
+*info, lucky_number = record
+print(info, lucky_number)
+
+# 利用星号表达式迭代元素为可变长元组的序列
+records = [('foo', 1, 2), ('bar', 'hello'), ('foo', 3, 4)]
+
+
+def do_foo(x, y):
+    print('foo', x, y)
+
+
+def do_bar(s):
+    print('bar', s)
+
+
+for tag, *args in records:
+    if tag == 'foo':
+        do_foo(*args)
+    elif tag == 'bar':
+        do_bar(*args)
+
+# 利用星号表达式分割字符串
+line = 'nobody:*:-2:-2:Unprivileged User:/var/empty:/usr/bin/false'
+name, *_, homedir, sh = line.split(":")
+print(name, homedir, sh)
+
+
