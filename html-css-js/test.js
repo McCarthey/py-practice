52ed68fb322693fd9cbc7bfd6321a913fe6d4ef3
Dean <mccarthey123@outlook.com>
2018-05-31 11:32:25 +0800

    Date.now()方法

--- html-css-js/test.js
+++ html-css-js/test.js
@@ -4,4 +4,5 @@ console.log(timeStamp.getTime())
 console.log(timeStamp.valueOf())
 console.log(Number(new Date()))
 console.log(+new Date())
+console.log(Date.now())
 console.log(Date.parse(timeStamp)) // 后三位是000 即 只精确到秒
