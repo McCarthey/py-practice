ab39281c367cc771e812cb3b5e430a4625ed6abf
mccarthey <mccarthey123@outlook.com>
2018-06-07 23:05:21 +0800

    safari的时间戳获取方法

--- html-css-js/test.js
+++ html-css-js/test.js
@@ -6,3 +6,7 @@ console.log(Number(new Date()))
 console.log(+new Date())
 console.log(Date.now())
 console.log(Date.parse(timeStamp)) // 后三位是000 即 只精确到秒
+
+// Safari浏览器器/ios app中打开网页 获取时间戳的方法需要兼容
+// new Date('xxxx-xx-xx xx:xx:xx')在上述情况下不适应 在safari下默认是无效的时间格式
+// 应使用new Date('xxxx/xx/xx xx:xx:xx')的兼容形式
