25de56833a3a421843823f905279b4603a265bdf
Dean <mccarthey123@outlook.com>
2018-07-07 16:37:16 +0800

    重构按钮响应事件函数 拆分成按钮按下、按钮松开两个函数

--- alien-game/game_functions.py
+++ alien-game/game_functions.py
@@ -4,24 +4,35 @@ import pygame
 
 def check_events(ship):
     # 用于响应鼠标和键盘事件
-	# 每当用户按键时，都将在pygame中注册一个事件。事件都是通过pygame.event.get()方法获取的
+        # 每当用户按键时，都将在pygame中注册一个事件。事件都是通过pygame.event.get()方法获取的
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
-				
+            check_keyup_events(event, ship)
+
+
+def check_keydown_events(event, ship):
+        # 响应按键
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+        # 响应松开
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def update_screen(ai_settings, screen, ship):
     # 用于更新屏幕
-	# 每次循环时都重绘屏幕 填充颜色
+    # 每次循环时都重绘屏幕 填充颜色
     screen.fill(ai_settings.bg_color)
     ship.blitme()
 
