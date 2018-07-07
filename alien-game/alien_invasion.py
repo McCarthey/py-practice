829abcd7022ef31e209986b3202c827ea7c68a68
Dean <mccarthey123@outlook.com>
2018-07-07 11:52:24 +0800

    将游戏的设置提出来

--- alien-game/alien_invasion.py
+++ alien-game/alien_invasion.py
@@ -1,12 +1,19 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    # 设置游戏窗口标题
     pygame.display.set_caption("Alien Invasion")
+    # 设置背景颜色 pygame中的颜色都是RGB的形式
+    bg_color = (230, 230, 230)
 
     # 开始游戏的主循环
     while True:
@@ -15,7 +22,11 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        # 让最近绘制的屏幕可见
+                # 每次循环时都重绘屏幕 填充颜色
+        screen.fill(ai_settings.bg_color)
+
+        # 让最近绘制的屏幕可见：每次执行循环时都会绘制一个空屏幕，并擦去旧屏幕，使得只有新屏幕可见，即是一个不断更新屏幕的过程，类似于电影的成像效果
         pygame.display.flip()
+
 
 run_game()
