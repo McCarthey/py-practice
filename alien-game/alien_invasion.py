36e20ff14bf41ba0ccc10dfb45abaf86c98b44c9
Dean <mccarthey123@outlook.com>
2018-07-07 15:58:22 +0800

    飞船控制

--- alien-game/alien_invasion.py
+++ alien-game/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -16,20 +17,12 @@ def run_game():
 
     # 创建一艘飞船
     ship = Ship(screen)
-    
+
     # 开始游戏的主循环
     while True:
-        # 监听输入事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-                # 每次循环时都重绘屏幕 填充颜色
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # 让最近绘制的屏幕可见：每次执行循环时都会绘制一个空屏幕，并擦去旧屏幕，使得只有新屏幕可见，即是一个不断更新屏幕的过程，类似于电影的成像效果
-        pygame.display.flip()
-
+        gf.check_events(ship)
+        # 飞船的位置将在检测到键盘事件后（更新屏幕前）更新
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
---
+++ alien-game/game_functions.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+
+
+def check_events(ship):
+    # 用于响应鼠标和键盘事件
+	# 每当用户按键时，都将在pygame中注册一个事件。事件都是通过pygame.event.get()方法获取的
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = False
+def update_screen(ai_settings, screen, ship):
+    # 用于更新屏幕
+	# 每次循环时都重绘屏幕 填充颜色
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # 让最近绘制的屏幕可见：每次执行循环时都会绘制一个空屏幕，并擦去旧屏幕，使得只有新屏幕可见，即是一个不断更新屏幕的过程，类似于电影的成像效果
+    pygame.display.flip()
