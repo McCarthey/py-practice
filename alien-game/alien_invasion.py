ab64d1e0c4ced9819937c225a0e77f5f9017eff8
Dean <mccarthey123@outlook.com>
2018-07-12 18:40:11 +0800

    重构子弹代码

--- alien-game/alien_invasion.py
+++ alien-game/alien_invasion.py
@@ -26,12 +26,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         # 飞船、子弹的位置将在检测到键盘事件后（更新屏幕前）更新
         ship.update()
-        bullets.update()
-        
-        # 删除已消失的子弹
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
+        gf.update_bullets(bullets)
         gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- alien-game/game_functions.py
+++ alien-game/game_functions.py
@@ -10,7 +10,7 @@ def check_events(ai_settings, screen, ship, bullets):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship, bullets, ai_settings,screen)
+            check_keydown_events(event, ship, bullets, ai_settings, screen)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
@@ -22,11 +22,7 @@ def check_keydown_events(event, ship, bullets, ai_settings, screen):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # 如果当前屏幕的子弹数小于最大限制 则创建一颗子弹，并将其加入到编组中
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
-
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
         # 响应松开
@@ -34,6 +30,23 @@ def check_keyup_events(event, ship):
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    # 如果当前屏幕的子弹数小于最大限制 则创建一颗子弹，并将其加入到编组中
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+
+def update_bullets(bullets):
+    # 更新子弹的位置，并删除已消失的子弹
+    bullets.update()
+
+    # 删除已消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
 
 
 def update_screen(ai_settings, screen, ship, bullets):
