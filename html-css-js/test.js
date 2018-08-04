f5bd77f4b3d67f4ec33e7c17900f94cbc37db5d8
Dean <mccarthey123@outlook.com>
2018-08-04 18:50:47 +0800

    rreduce

--- html-css-js/test.js
+++ html-css-js/test.js
@@ -1,12 +1,11 @@
-// 获取当前时间戳的方法
-let timeStamp = new Date()
-console.log(timeStamp.getTime())
-console.log(timeStamp.valueOf())
-console.log(Number(new Date()))
-console.log(+new Date())
-console.log(Date.now())
-console.log(Date.parse(timeStamp)) // 后三位是000 即 只精确到秒
+const posts = [
+    { id: 1, upVotes: 2 },
+    { id: 2, upVotes: 89 },
+    { id: 3, upVotes: 1 }
+]
 
-// Safari浏览器器/ios app中打开网页 获取时间戳的方法需要兼容
-// new Date('xxxx-xx-xx xx:xx:xx')在上述情况下不适应 在safari下默认是无效的时间格式
-// 应使用new Date('xxxx/xx/xx xx:xx:xx')的兼容形式
+const totalUpvotes = posts.reduce((totalUpvotes, currentPost) => {
+	totalUpvotes + currentPost.upVotes, 0
+})
+
+console.log(totalUpvotes)
