bd4c770052a65cb9ea8f69a8a0a373dac936fff8
Dean <mccarthey123@outlook.com>
2018-08-18 17:39:49 +0800

    alg-- two num

---
+++ algorithm/array/two_sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+                """
+        for i in range(len(nums)):
+            num_expect = target - nums[i]
+            if num_expect in nums[i + 1:]:
+                print([i, nums.index(num_expect, i+1)])
+                return [i, nums.index(num_expect, i+1)]
+
+Solution().twoSum([3, 5,4,1,4], 9)
