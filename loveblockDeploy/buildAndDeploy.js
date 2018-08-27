8398489ebad63cefab688a9237277dbfd3e26e83
Dean <mccarthey123@outlook.com>
2018-08-27 18:37:05 +0800

    更新自动部署脚本

--- loveblockDeploy/buildAndDeploy.js
+++ loveblockDeploy/buildAndDeploy.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const { exec } = require('child_process')
+const {exec} = require('child_process')
 const fse = require('fs-extra')
-const fs = require('fs')
 const path = require('path')
+// const open = require('open')
 
 // yarn run build-dev -e ddd
 const argvs = process.argv.slice(2)
@@ -49,32 +49,49 @@ exec(buildCommand, (err, stdout, stderr) => {
     }
     // console.log(lbHtmlDir)
     // 将dist/中的html文件复制到loveblockWeb/htmlroottest目录
-    fse.copySync(distHtml, lbHtmlDir, { overwrite: true })
-    fse.copySync(distSW, lbSW, { overwrite: true })
+    fse.copySync(distHtml, lbHtmlDir, {overwrite: true})
+    fse.copySync(distSW, lbSW, {overwrite: true})
 
-	// git提交
-    if (mode === 'test') throw new Error('用于测试的包不上传至git')
-
-    const gitCommand = `git st && git add . && git ci -m "${mode} ${+new Date()}" && git push`
-
-    exec(
-        gitCommand, {
-            cwd: '../loveblockweb/'
-        },
-        (gitErr, gitStdout, gitStderr) => {
-            if (gitErr) {
-                console.log('没有可以更新的内容')
-                throw new Error('停止执行')
+    // 测试指令不进行git提交
+    if (mode === 'test') {
+        // throw new Error('用于测试的包不上传至git')
+        // 开启测试服务器sss -s
+        exec('sss -s', {
+            cwd: '../loveblockweb/htmlroottest'
+        }, (sErr, sStdout, sStderr) => {
+            if (sErr) {
+                throw new Error('启动测试服务器失败')
             }
-            console.log(gitStdout)
-            console.log(gitStderr)
-            console.log('git push 完成')
+        });
+        // TODO:暂时只能设置5s后执行打开浏览器的操作 不知道有没有好办法（继续学习……）
+        setTimeout(() => {
+            console.log('启动测试服务器 Ctrl + C 停止')
+            exec(`start "%ProgramFiles%\Google\Chrome\Application\chrome.exe" http://localhost:8099/${entry}.html`)
+        }, 5000)
+    } else {
+        // git提交
+        const gitCommand = `git pull && git add . && git ci -m "auto push ${entry}.html: ${mode} ${+new Date()}" && git push`
 
-            // 如果是正式环境 需要两个get请求才能更新成功
-            if (mode === 'build') {
-                exec('curl http://normal1.loveblock.one:9100')
-                exec('curl http://normal2.loveblock.one:9100')
+        exec(
+            gitCommand, {
+                cwd: '../loveblockweb/'
+            },
+            (gitErr, gitStdout, gitStderr) => {
+                if (gitErr) {
+                    console.log('没有可以更新的内容')
+                    throw new Error('停止执行')
+                }
+                console.log(gitStdout)
+                console.log(gitStderr)
+                console.log(`${mode}环境 git push 完成`)
+
+                // 如果是正式环境 需要两个get请求才能更新成功
+                if (mode === 'build') {
+                    console.log('正在执行正式环境更新请求，请稍后')
+                    exec('curl http://normal1.loveblock.one:9100')
+                    exec('curl http://normal2.loveblock.one:9100')
+                }
             }
-        }
-    )
+        )
+    }
 })
