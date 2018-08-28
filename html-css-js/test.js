4b4b96c1360b73c38ed577c6149befbedada4c96
Dean <mccarthey123@outlook.com>
2018-08-28 18:50:16 +0800

    arguments callee  apply call

--- html-css-js/test.js
+++ html-css-js/test.js
@@ -5,7 +5,108 @@ const posts = [
 ]
 
 const totalUpvotes = posts.reduce((totalUpvotes, currentPost) => {
-	totalUpvotes + currentPost.upVotes, 0
+    totalUpvotes + currentPost.upVotes, 0
 })
 
 console.log(totalUpvotes)
+
+
+function factorial(num) {
+    if (num < 1) {
+        return 1
+    } else {
+        return num * factorial(num - 1)
+    }
+}
+
+/* 
+    可以使用arguments.callee，解决函数名紧耦合的问题 
+    arguments.callee是一个指针，指向拥有这个arguments对象的函数
+    但是可读性差，已不推荐使用
+*/
+
+function factorial(num) {
+    if (num < 1) {
+        return 1
+    } else {
+        return num * arguments.callee(num - 1)
+    }
+}
+
+const trueFactorial = factorial
+
+factorial = function() {
+    return 0
+}
+
+trueFactorial(4)
+
+/**
+ * 函数对象的属性：calller。这个属性中保存着调用当前函数的函数的引用，如果在全局作用域中调用当前函数，它的值为null
+ */
+function outer() {
+    inner()
+}
+
+function inner() {
+    console.log(inner.caller) // 会打印inner的调用者--outer函数的源代码
+    console.log(arguments.callee.caller) // 同上(严格模式无法使用caller,callee,arguments)
+}
+
+outer()
+
+
+/**
+ * 函数属性和方法
+ * 函数的length表示函数希望接受的命名参数的个数
+ */
+
+function sayName(name) {
+    console.log(name)
+}
+
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+function sayHi() {
+    console.log('hi')
+}
+
+console.log(sayName.length, sum.length, sayHi.length)
+
+/**
+ * 每个函数都包含两个方法：apply()和call()
+ * 这两个方法都是在特定的作用域中调用函数，实际上等同于设置函数体内this对象的值
+ * apply接受参数数组,call参数需要明写出来
+ */
+
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+function callSum1(num1, num2) {
+    return sum.apply(this, arguments)
+}
+
+function callSum2(num1, num2) {
+    return sum.call(this, num1 ,num2)
+}
+
+console.log(callSum1(10, 10))
+console.log(callSum2(10, 10))
+
+ /**
+  * 使用apply()和call()扩充函数运行的作用域
+  */
+window.color = 'red'
+var o = {color: 'blue'}
+function sayColor() {
+    console.log(this.color)
+}
+
+sayColor()
+
+sayColor.call(this)
+sayColor.call(window)
+sayColor.call(o)
