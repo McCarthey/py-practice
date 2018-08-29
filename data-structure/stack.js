04100e6cb235fa01848c7b2c5fcd61c12280eabc
Dean <mccarthey123@outlook.com>
2018-08-29 10:56:00 +0800

    stack (ES5/6)

---
+++ data-structure/stack.js
@@ -0,0 +1,65 @@
+/**
+ * 栈 - 后进先出
+ * 需要几个方法: push pop peek isEmpty clear size
+ */
+function Stack() {
+    var items = [];
+    this.push = function(element) {
+        items.push(element) // 新元素放在末尾（栈顶）
+    }
+    this.pop = function(element) {
+        return items.pop(element) // 弹出并返回末尾的元素
+    }
+    this.peek = function() {
+        return items[items.length - 1] // 返回最后入栈的元素
+    }
+    this.isEmpty = function() {
+        return items.length === 0 // 判断栈是否为空
+    }
+    this.size = function() {
+		return items.length // 返回栈的大小
+	}
+	this.clear = function () {
+		items = [] // 把栈清空
+	}
+	this.print = function () {
+		console.log(items.toString())
+	}
+}
+
+var stack = new Stack()
+
+/**
+ * ES6 的class实现
+ */
+class Stack {
+	constructor(items) {
+		this.items = items
+	}
+	
+	push(element) {
+		this.items.push(element)
+	}
+	
+	pop(element) {
+		return this.items.pop(element)
+	}
+	
+	peek() {
+		return this.items[this.items.length - 1]
+	}
+	
+	isEmpty() {
+		return this.items.length === 0
+	}
+	
+	clear() {
+		this.items = []
+	}
+	
+	size() {
+		return this.items.length
+	}
+}
+
+const stack = new Stack([])
