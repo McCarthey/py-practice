79b9b1876bf84c4387086cf0581246b1d0ddf318
Dean <mccarthey123@outlook.com>
2018-09-14 16:58:41 +0800

    冒泡算法：如果当前项比后一项大 ，则交换位置，复杂度O(n2)

--- algorithm/JS-al/bubble.js
+++ algorithm/JS-al/bubble.js
@@ -1,9 +1,32 @@
 function ArrayList() {
-	var array = []
-	this.insert = function (item) {
-		array.push(item)
-	}
-	this.toString = function () {
-		return array.join()
+    var array = []
+    this.insert = function(item) {
+        array.push(item)
+    }
+    this.toString = function() {
+        return array.join()
+    }
+    // 冒泡算法，性能最差
+    this.bubbleSort = function() {
+        for (var i = 0; i < array.length; i++) {
+            for (var j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(array, j, j + 1)
+                }
+            }
+        }
 	}
 }
+
+function swap(arr, j, k) {
+    ;[arr[j], arr[k]] = [arr[k], arr[j]]
+}
+
+// 测试
+var array = new ArrayList()
+array.insert(3)
+array.insert(8)
+array.insert(1)
+array.insert(9)
+array.insert(0)
+array.insert(5)
