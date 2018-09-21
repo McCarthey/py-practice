d640c7ace3e4fdb1595c0a753f4705daa85f5ced
Dean <mccarthey123@outlook.com>
2018-09-21 16:39:27 +0800

    使用weakmap实现私有属性

--- html-css-js/shape.js
+++ html-css-js/shape.js
@@ -1,14 +1,24 @@
+// 使用WeakMap来存储所有私有值
+const map = new WeakMap()
+
+// 创建一个在每个实例中存储私有变量的对象
+const internal = obj => {
+	if (!map.has(obj)) {
+		map.set(obj, {})
+	}
+	return map.get(obj)
+}
 class Shape {
 	constructor(width, height) {
-		this._width = width
-		this._height = height
+		internal(this).width = width
+		internal(this).height = height
 	}
 	
 	get area() {
-		return this._width * this._height
+		return internal(this).width * internal(this).height
 	}
 }
 
 const square = new Shape(10, 10)
 console.log(square.area)
-console.log(square._width)
+console.log(map.get(square))
