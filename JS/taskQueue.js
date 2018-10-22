9be3d7c5914f9bea7e9e08df31a78df4c5895bdd
Dean <mccarthey123@outlook.com>
2018-10-22 11:09:31 +0800

    microtask macrotask

---
+++ JS/taskQueue.js
@@ -0,0 +1,51 @@
+/**
+ * 任务队列：
+ * microtasks:
+ * - process.nextTick
+ * - promise
+ * - Object.observe
+ * macrotasks:
+ * - setTimeout
+ * - setInterval
+ * - setImmediate
+ * - I/O
+ */
+console.log('start')
+
+const interval = setInterval(() => {
+	console.log('setInterval')
+}, 0)
+
+setTimeout(() => {
+	console.log('setTimeout 1')
+	Promise.resolve()
+		.then(() => {
+			console.log('promise 3')
+		})
+		.then(() => {
+			console.log('promise 4')
+		})
+		.then(() => {
+			setTimeout(() => {
+				console.log('setTimeout 2')
+				Promise.resolve()
+					.then(() => {
+						console.log('promise 5')
+					})
+					.then(() => {
+						console.log('promise 6')
+					})
+					.then(() => {
+						clearInterval(interval)
+					})
+			}, 0)
+		})
+}, 0)
+
+Promise.resolve()
+	.then(() => {
+		console.log('promise 1')
+	})
+	.then(() => {
+		console.log('promise 2')
+	})
