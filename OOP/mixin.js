04e93401996ac58b6a6e7b4c6e34a45d35c5bd88
Dean <mccarthey123@outlook.com>
2018-10-22 18:27:33 +0800

    显式伪多态

--- OOP/mixin.js
+++ OOP/mixin.js
@@ -1,5 +1,6 @@
 /**
  * 显式混入：实现将父类复制到子类的功能
+ * 显式伪多态：在JS中，需在所有需要使用伪多态引用的地方创建一个函数关联，导致代码可读性差且难以维护，应尽量避免
  */
 function mixin(sourceObj, targetObj) {
 	for (var key in sourceObj) {
@@ -28,7 +29,11 @@ var Car = mixin(Vehicle, {
 	wheels: 4,
 	
 	drive() {
-		Vehicle.drive.call(this)
+		Vehicle.drive.call(this) // 显式多态（调用super可称之为相对多态），并调用.call(this)使得drive函数在Car对象的上下文中执行
 		console.log("Rolling on all " + this.wheels)
 	}
 })
+
+/**
+ * 混合复制
+ */
