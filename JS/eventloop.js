9081da359770a2b80d332721913a1d5d910683d1
Dean <mccarthey123@outlook.com>
2018-10-22 10:17:32 +0800

    express + superagent eventloop

---
+++ JS/eventloop.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const superagent = require('superagent')
+const app = express()
+
+app.get('/', getArticle)
+
+function getArticle(req, res) {
+	fetchArticle(req, res)
+	print()
+}
+
+const aids = [4564824, 4506868, 4767667, 4856099, 7456996]
+
+function fetchArticle(req, resp) {
+	const aid = aids[Math.floor(Math.random() * aids.length)]
+	superagent.get(`http://news-at.zhihu.com/api/4/news/${aid}`)
+		.end((err, res) => {
+			if (err) {
+				console.log('error...')
+				return res.status(500).send('an error...')
+			}
+			const article = res.body
+			console.log(article)
+			resp.send(article)
+			console.log('Got an article')
+		})
+	console.log('Now is fetching an article')
+}
+
+function print() {
+	console.log('Print something')
+}
+
+app.listen('5000')
+
+/**
+ * 虽然V8是单线程的，但底层的C++ API却不是。当我们执行一些非阻塞的操作，Node会调用一些代码（libuv），与引擎里js代码同时执行。一旦这个隐藏的线程收到了等到的返回值或者抛出一个异常，之前提供的回调函数就会执行
+ */
--- mongoDB/README.md
+++ mongoDB/README.md
@@ -7,5 +7,7 @@
 	db /*显示你正在使用的db*/
 	show dbs /*列出全部的db*/
 	use <database> /*切换到指定的db*/
+	db.myCollection.insertOne( { x: 1 } ) /*在集合myCollection中插入数据*/
+	db.getCollection('myCollection').find() /*列出myCollection中的数据*/
 	```
 - MongoDB在集合(collections)中存储BSON文档(BSON documents);集合(collections)存储在数据库(databases)中。在类似的数据中，集合相当于表。
