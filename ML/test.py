9e953913aab6999f79426a9aa8e4281a2bc12f12
mccarthey <mccarthey123@outlook.com>
2018-11-08 23:53:54 +0800

    稀疏矩阵

--- ML/test.py
+++ ML/test.py
@@ -5,6 +5,12 @@ from scipy import sparse
 x = np.array([[1, 2, 3],[4, 5, 6]])
 print("x:\n{}".format(x))
 
-# 创建一个二维NumPy数组，对角线为1，其余都为0
+# 创建一个二维NumPy数组，对角线为1，其余都为0，也是稀疏矩阵
 eye = np.eye(4)
 print("Numpy Array:\n{}".format(eye))
+
+# 将Numpy数组转换为CSR格式的SciPy稀疏矩阵
+# 只保存非零元素
+sparse_matrix = sparse.csr_matrix(eye)
+print("\nSciPy sparce CSR matrix:\n{}".format(sparse_matrix))
+
