9e6b103ad37656ec309fcdc92a221f09844341a4
Dean <mccarthey123@outlook.com>
2018-11-15 15:49:18 +0800

    模板插值

--- vue-ssr/useTemplate.js
+++ vue-ssr/useTemplate.js
@@ -11,9 +11,19 @@ server.get('*', (req, res) => {
         },
         template: `<div>访问的URL是：{{ url }}</div>`
     })
-
-    renderer.renderToString(app, (err, html) => {
-        console.log(html) // html 将是注入应用程序内容的完整页面
+	
+	// 我们可以通过传入一个"渲染上下文对象"，作为 renderToString 函数的第二个参数，来提供插值数据：
+	const context = {
+		title: 'hello',
+		meta: `
+			<meta name="author" content="mccarthey">
+			<meta charset="utf-8">
+    		<meta name="description" content="this is for test">
+		`
+	}
+    renderer.renderToString(app, context, (err, html) => {
+		res.end(html)
+		console.log(html) // html 将是注入应用程序内容的完整页面
     })
 })
 
