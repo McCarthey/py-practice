bba10212ec6c0b80c1a9a27ed639ac625dc2866a
Dean <mccarthey123@outlook.com>
2018-11-15 15:30:14 +0800

    vue ssr demo

---
+++ vue-ssr/index.js
@@ -0,0 +1,20 @@
+// 第一步：创建一个Vue实例
+const Vue = require('vue')
+const app = new Vue({
+	template: `<div>Hello World</div>`
+})
+
+// 第二步：创建一个renderer
+const renderer = require('vue-server-renderer').createRenderer()
+
+// 第三步：将Vue实例渲染为HTML
+renderer.renderToString(app, (err, html) => {
+	if (err) throw err
+	console.log(html)
+})
+
+renderer.renderToString(app).then(html => {
+	console.log(html)
+}).catch(err => {
+	console.log(err)
+})
