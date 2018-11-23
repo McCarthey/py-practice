bed40c5961404452f83242149fa0002100dee1f3
Dean <mccarthey123@outlook.com>
2018-11-23 20:48:39 +0800

    ts 基础类型

---
+++ ts/src/test.ts
@@ -0,0 +1,15 @@
+// 布尔
+let isDone: boolean = false;
+
+// 数字
+let decLiteral: number = 6;
+let hexLiteral: number = 0xf00d;
+let binaryLiteral: number = 0b1010;
+let octalLiteral: number = 0o744;
+
+// 字符串
+let personName: string = 'bob';
+let age: number = 37;
+let sentence: string = `Hello, my name is ${personName}.
+
+I'll be ${ age + 1} years old next month.`;
