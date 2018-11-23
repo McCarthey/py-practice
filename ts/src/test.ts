7b5fb5fed38726ba053acb3bfde5b9634a37197b
Dean <mccarthey123@outlook.com>
2018-11-23 20:52:06 +0800

    基本类型

--- ts/src/test.ts
+++ ts/src/test.ts
@@ -13,3 +13,13 @@ let age: number = 37;
 let sentence: string = `Hello, my name is ${personName}.
 
 I'll be ${ age + 1} years old next month.`;
+
+// 数组
+let list: number[] = [1, 2, 3];
+// 数组泛型
+let anotherList: Array<number> = [1, 2, 3];
+
+// 元组：各元素的类型不必相同
+let x: [string, number];
+x = ['hello', 10];
+x = [10, 'hello']; // Error
