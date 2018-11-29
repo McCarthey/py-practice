39aa4e2dceb3676e2c6b5faacb0f209e00192649
Dean <mccarthey123@outlook.com>
2018-11-29 11:34:02 +0800

    test emoji

--- JS/emoji.js
+++ JS/emoji.js
@@ -1,2 +1,13 @@
-const emoji = require('node-emoji')
-emoji.get('coffee')
+const emojiRegex = require('emoji-regex')
+const regex = emojiRegex()
+const text = `
+\u{231A}: ⌚ default emoji presentation character (Emoji_Presentation)
+\u{2194}\u{FE0F}: ↔️ default text presentation character rendered as emoji
+\u{1F469}: 👩 emoji modifier base (Emoji_Modifier_Base)
+\u{1F469}\u{1F3FF}: 👩🏿 emoji modifier base followed by a modifier
+`;
+let match;
+while (match = regex.exec(text)) {
+	const emoji = match[0];
+	console.log(`Matched sequence ${emoji} — code points: ${[...emoji].length}`);
+}
