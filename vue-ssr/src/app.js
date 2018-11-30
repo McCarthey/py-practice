dee1afd76331f57681c708b87fbeef680da9062e
Dean <mccarthey123@outlook.com>
2018-11-30 19:56:55 +0800

    ssr router

--- vue-ssr/src/app.js
+++ vue-ssr/src/app.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {createRouter} from './router'
 
 // 导出一个工厂函数，用于创建新的应用程序，router和store实例
 export function createApp() {
+	// 创建 router 实例
+	const router = createRouter()
+	
 	const app = new Vue({
 		// 根实例简单的渲染应用程序组件
+		// 注入 router 到根 Vue 实例
+		router,
 		render: h => h(App)
 	})
-	return { app }
+	return { app ,router }
 }
