95863fda5867eeb1257474e2395d2e62d5c7224b
Dean <mccarthey123@outlook.com>
2018-12-06 18:21:32 +0800

    set实现交、并、差集

---
+++ JS/set.js
@@ -0,0 +1,12 @@
+// set实现交、并、差集
+let a = new Set([1, 2, 3])
+let b = new Set([4, 3, 2])
+
+// 并集
+let union = new Set([...a, ...b])
+
+// 交集
+let intersect = new Set([...a].filter(x => b.has(x)))
+
+// 差集
+let difference = new Set([...a].filter(x => !b.has(x)))
