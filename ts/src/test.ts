42619c722f2ea21e2af3133367487df982b6f860
mccarthey <mccarthey123@outlook.com>
2018-12-12 23:34:54 +0800

    类型断言

--- ts/src/test.ts
+++ ts/src/test.ts
@@ -81,3 +81,16 @@ create({pop: 0})
 create(null)
 
 // create(42) // Error
+
+/* 类型断言:
+通过类型断言这种方式可以告诉编译器，“相信我，我知道自己在干什么”。 
+类型断言好比其它语言里的类型转换，但是不进行特殊的数据检查和解构。 
+它没有运行时的影响，只是在编译阶段起作用。 TypeScript会假设你，程序员，已经进行了必须的检查。
+*/
+let someValue: any = 'this is a string'
+let strLength: number = (<string>someValue).length // 尖括号断言
+
+let someValue2: any = 'this is a string'
+let strLength2: number = (someValue2 as string).length // as 语法
+
+
