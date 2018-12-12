de1643c03b569161d9ca54fa987382bd15a72ee1
mccarthey <mccarthey123@outlook.com>
2018-12-12 23:06:24 +0800

    any

--- ts/src/test.ts
+++ ts/src/test.ts
@@ -26,7 +26,7 @@ x = ['hello', 10];
 console.log(x[0].substr(1))
 // console.log(x[1].substr(1)) // Error,'number' does not have 'substr'
 
-x[2] = 'world'; // ok, 字符串可以赋值给(string | number)类型
+// x[2] = 'world'; // ok, 字符串可以赋值给(string | number)类型
 console.log(x[1].toString()) // ok, 'string'和'number'都有toString方法
 // x[6] = true; // Error, 布尔不是(string | number)类型 
 
@@ -42,7 +42,10 @@ let colorName: string = Color[4]
 
 console.log(colorName)
 
-// Any
+// Any 当你只知道一部分数据的类型时，any类型也是有用的。 比如，你有一个数组，它包含了不同的类型的数据：
 let notSure: any = 4
 notSure = "maybe a string instead"
 notSure = false
+
+let anyList: any[] = [1, true, "free"]
+anyList[1] = 100
