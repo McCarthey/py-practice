892d9ff2f602b0ec2fded75c1a02784ae4178cd9
mccarthey <mccarthey123@outlook.com>
2018-12-12 23:11:06 +0800

    void

--- ts/src/test.ts
+++ ts/src/test.ts
@@ -49,3 +49,9 @@ notSure = false
 
 let anyList: any[] = [1, true, "free"]
 anyList[1] = 100
+
+// Void
+// 某种程度上来说，void类型像是与any类型相反，它表示没有任何类型。 当一个函数没有返回值时，你通常会见到其返回值类型是 void：
+function warnUser(): void {
+  console.log('This is my warning message');
+}
