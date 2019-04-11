270fa59c9211f9309186f8f5dc1a7a76057e11de
Randall <randall@duomai.com>
2019-04-11 12:54:21 +0800

    [add] useState hook in react

---
+++ react-learning/src/hooks/useState/no-useState.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import '../style.css'
+
+interface countProps {}
+interface countState {
+    count: number
+    name: string
+}
+
+class App extends React.Component<countProps, countState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      count: 0,
+      name: "test"
+    };
+  }
+  render() {
+    const { count } = this.state;
+    return (
+      <div className="count-wrap">
+        <p>Count: {count}</p>
+        <button onClick={() => this.setState({ count: count + 1 })}>+</button>
+        <button onClick={() => this.setState({ count: count - 1 })}>-</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- react-learning/src/index.ts
+++ react-learning/src/index.ts
@@ -1,1 +1,2 @@
-import './hooks/useState.tsx'
+// import './hooks/useState.tsx'
+import './hooks/useState/no-useState.tsx'
