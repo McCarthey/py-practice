f197e98fed13678ced0c834a4b1cd84bad292be5
Randall <randall@duomai.com>
2019-06-05 15:09:25 +0800

    [add] koa

--- java-practice/HelloWorld.java
+++ java-practice/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String []args) {
         System.out.println("Hello world");
+        System.out.println(0.1 + 0.2 != 0.3);
+        System.out.println(Byte.SIZE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
     }
 }
