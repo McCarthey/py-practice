f54db5d72b3d24ca647cf82ac6204b5bb20ba3e2
Randall <randall@duomai.com>
2019-06-06 13:53:28 +0800

    [add] @HttpCode @Header

--- nest-practice/src/cats/cats.controller.ts
+++ nest-practice/src/cats/cats.controller.ts
@@ -1,19 +1,29 @@
-import { Controller, Get, Req, Post } from '@nestjs/common';
+import { Controller, Get, Req, Post, HttpCode, Header } from '@nestjs/common';
 import { Request } from 'express';
-import { create } from 'domain';
 
 @Controller('cats')
 export class CatsController {
   @Post()
+  @HttpCode(204)
   create(): string {
       return 'This action adds a new cat\n'
   }
   @Get()
+  @Header('Cache-Control', 'none')
   findAll(@Req() request: Request): string {
     return 'This action returns all cats\n'
+  }
+  @Get('ab*cd')
+  findWildCat():string {
+      return 'This route uses a wildcard\n'
   }
 }
 
 /**
  * controller与装饰器的组合形成了路由，上述路由为 /cats
+ * 通过curl测试：
+ *      get: curl http://localhost:3000/cats
+ *      post: curl -d {} http://localhost:3000/cats
+ * 
+ * 通配符路由为 /cats/ab123cd
  */
