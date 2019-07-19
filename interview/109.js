cbf83409c611efa9370ecf81efdad7cf3c00e44e
Randall <randall@duomai.com>
2019-07-19 13:46:06 +0800

    [add] daily question #109

---
+++ interview/109.js
@@ -0,0 +1,10 @@
+var name = 'Tom';
+(function() {
+    if (typeof name == 'undefined') {
+        name = 'Jack';
+        console.log('Goodbye ' + name);
+    } else {
+        console.log('Hello ' + name);
+    }
+})();
+// 'Hello Tom'
