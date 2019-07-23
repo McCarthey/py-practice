854f0f6205e96b32e00952d861c4d459808447d2
Randall <randall@duomai.com>
2019-07-23 11:14:36 +0800

    [add] daily question #111

---
+++ interview/111.js
@@ -0,0 +1,38 @@
+// 转换对象
+var entry = {
+    a: {
+        b: {
+            c: {
+                dd: 'abcdd'
+            }
+        },
+        d: {
+            xx: 'adxx'
+        },
+        e: 'ae'
+    }
+}
+
+// 要求转换成如下对象
+var output = {
+    'a.b.c.dd': 'abcdd',
+    'a.d.xx': 'adxx',
+    'a.e': 'ae'
+}
+
+function flatObj(obj, parentKey = '', result = {}) {
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            let keyStr = `${parentKey}${key}`
+            if (Object.prototype.toString.call(obj[key]) === '[object Object]') {
+                flatObj(obj[key], keyStr + '.', result)
+            } else {
+                result[keyStr] = obj[key]
+            }
+        }
+    }
+    return result
+}
+
+const res = flatObj(entry)
+console.log(res)
