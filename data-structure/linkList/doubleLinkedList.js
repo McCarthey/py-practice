7277318fe31bd3ee7d21c9417cbdee68de339f0a
Randall <randall@duomai.com>
2019-07-30 13:47:37 +0800

    [add] 双向链表插入数据优化

--- data-structure/linkList/doubleLinkedList.js
+++ data-structure/linkList/doubleLinkedList.js
@@ -3,21 +3,21 @@
  * 双向链表可以从头到尾迭代，也可以反过来
  */
 function DoubleLinkedList() {
-    var Node = function(element) {
+    let Node = function (element) {
         this.element = element
         this.next = null
         this.prev = null // 指向前一个元素
     }
 
-    var length = 0
-    var head = null
-    var tail = null // 指向尾元素
+    let length = 0
+    let head = null
+    let tail = null // 指向尾元素
 
     // 向特定位置插入一个新的项
-    this.insert = function(position, element) {
+    this.insert = function (position, element) {
         // 检查越界值
         if (position >= 0 && position <= length) {
-            var node = new Node(element),
+            let node = new Node(element),
                 current = head,
                 previous,
                 index = 0
@@ -32,18 +32,29 @@ function DoubleLinkedList() {
                     head = node
                 }
             } else if (position === length) { // 在最后一个位置添加
-				node.prev = current
-				current.next = node
-				tail = node
-            } else {
+                current = tail
+                current.next = node
+                node.prev = current
+                tail = node
+            } else if (position <= length / 2) { // 从头开始遍历
                 while (index++ < position) { // index++ 结果为当前index值，然后index再+1。例如，当index = 0, position = 1时，循环会执行一次，循环结束后index = 1
                     previous = current
                     current = current.next
                 }
-				node.next = current
-				previous.next = node
-				current.prev = node
-				node.prev = previous
+                node.next = current
+                previous.next = node
+                current.prev = node
+                node.prev = previous
+            } else { // 当position大于长度的一半的时候从尾部开始遍历，减少查找次数 TODO:
+                current = tail
+                while(index++ < length - position) {
+                    previous = current
+                    current = current.prev
+                }
+                node.next = previous
+                previous.prev = node
+                current.next = node
+                node.prev = current
             }
 
             length++
@@ -51,5 +62,11 @@ function DoubleLinkedList() {
         } else {
             return false
         }
+    }
+    this.removeAt = function (position, element) {
+
+    }
+    this.getLen = function () {
+        return length
     }
 }
