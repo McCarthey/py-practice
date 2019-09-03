f526de9e1511c7e96e8a18d32b34310fd4a631fc
Randall <randall@duomai.com>
2019-09-03 13:57:08 +0800

    [add] setTimeout vs setInterval

--- interview/133.js
+++ interview/133.js
@@ -10,3 +10,9 @@ function mySetInterval(func, ms) {
     return timer
 }
 
+/**
+ * setTimeout VS setInterval
+ */
+// setInterval定时器的缺点: 某些间隔可能会被跳过；可能多个定时器会连续执行；（因为只有当队列中没有该定时器的其他实例时，才将定时器代码添加到实例中）。
+// setTimeout实现定时器的好处：在一个定时器执行完前，不会向队列中插入新的定时器；保证定时器间隔；
+
