71004bb97a3c690b7c4a55808a4c2c3396afe410
Randall <randall@duomai.com>
2019-09-04 10:02:20 +0800

    [add] Date.parse(date)也可以生成时间戳

--- interview/134.js
+++ interview/134.js
@@ -2,11 +2,11 @@
  * 如 2015-2-8 到 2015-3-3，返回【2015-2-8 2015-2-9...】
  */
 function getDates(startDate, endDate) {
-    let startDateStamp = +new Date(startDate)
-    const endDateStamp = +new Date(endDate)
+    let startDateStamp = +new Date(startDate) // 可以使用 Date.parse(startDate) 完成日期的解析
+    const endDateStamp = +new Date(endDate) // 可以使用 Date.parse(endDate) 完成日期的解析
     const DAY_MS = 86400000
     const result = []
-    while(startDateStamp < endDateStamp) {
+    while(startDateStamp <= endDateStamp) {
         const date = (new Date(startDateStamp).toLocaleDateString()).replace(/\//g,'-')
         result.push(date)
         startDateStamp += DAY_MS
