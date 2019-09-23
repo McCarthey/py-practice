bd1c179fa07c37fdd81f87f841caa97f367d7e3a
Randall <randall@duomai.com>
2019-09-23 09:48:06 +0800

    [add] daily question #139

---
+++ interview/139.js
@@ -0,0 +1,5 @@
+/**
+ * NextTick原理：
+ * nextTick可以让我们在下次DOM更新循环结束之后执行延迟回调，用于获得更新后的DOM 
+ * 参考：https://github.com/InterviewMap/CS-Interview-Knowledge-Map/blob/master/Framework/vue-zh.md#nexttick-原理分析
+ */
