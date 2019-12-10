302fbcd70fa9fb7129c968aa034d5bd32a6d9088
Randall <randall@duomai.com>
2019-12-10 15:05:01 +0800

    [add] sort(): compare function

--- algorithm/JS-al/sort.js
+++ algorithm/JS-al/sort.js
@@ -1,16 +1,16 @@
 function ArrayList() {
     var array = []
-    this.insert = function(item) {
+    this.insert = function (item) {
         array.push(item)
     }
-    this.toString = function() {
+    this.toString = function () {
         return array.join()
     }
-    var swap = function(arr, j, k) {
+    var swap = function (arr, j, k) {
         [arr[j], arr[k]] = [arr[k], arr[j]]
     }
     // 冒泡算法，性能最差
-    this.bubbleSort = function() {
+    this.bubbleSort = function () {
         for (var i = 0; i < array.length; i++) {
             for (var j = 0; j < array.length - 1; j++) {
                 if (array[j] > array[j + 1]) {
@@ -20,7 +20,7 @@ function ArrayList() {
         }
     }
     // 改进后的冒泡算法: 已排序后的不进行比较
-    this.modifyBubbleSort = function() {
+    this.modifyBubbleSort = function () {
         for (var i = 0; i < array.length; i++) {
             for (var j = 0; j < array.length - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
@@ -34,7 +34,7 @@ function ArrayList() {
      * 假定最小值索引值，然后遍历，更新最小值索引，如果最小值索引已更新，则交换原索引和新索引，即找到当前列表的最小值
      * 循环以上步骤（最后一次不必执行，因为列表中只有最后一项，必然已经排序）
      */
-    this.selectionSort = function() {
+    this.selectionSort = function () {
         var indexMin
         for (var i = 0; i < array.length - 1; i++) {
             indexMin = i
@@ -53,7 +53,7 @@ function ArrayList() {
      * 算法假定第一项已排序，从第二项开始，将每项的索引和值赋给两个临时变量
      * 如果前一项大于当前项的值，则将前一项的值赋给当前项，
      */
-    this.insertionSort = function() {
+    this.insertionSort = function () {
         var j, temp
         for (var i = 1; i < array.length; i++) {
             j = i
@@ -69,8 +69,8 @@ function ArrayList() {
      * 归并排序：分治算法，将原始数组切分成较小的数组，直到每个小数组只有一个位置，接着将小数组归并为较大的数组，直到最后只有一个排序完毕的大数组
      * 归并时候，采用双指针，每次比较都将两个数组中较小值推入结果中，一旦有一个数组遍历完，就把剩下的数组的元素依次推入结果中
      */
-    this.mergeSort = function() {
-        var mergeSortRec = function(array) {
+    this.mergeSort = function () {
+        var mergeSortRec = function (array) {
             var length = array.length
             if (length === 1) {
                 return array
@@ -82,7 +82,7 @@ function ArrayList() {
             return merge(mergeSortRec(left), mergeSortRec(right))
         }
 
-        var merge = function(left, right) {
+        var merge = function (left, right) {
             var result = [],
                 il = 0,
                 ir = 0
@@ -117,8 +117,8 @@ function ArrayList() {
      * 
      * （注：主元的选择将影响算法的表现）
      */
-    this.quickSort = function() {
-        var quick = function(array, left, right) {
+    this.quickSort = function () {
+        var quick = function (array, left, right) {
             var index
             if (array.length > 1) {
                 index = partition(array, left, right)
@@ -126,13 +126,13 @@ function ArrayList() {
                 if (left < index - 1) {
                     quick(array, left, index - 1)
                 }
-                
+
                 if (index < right) {
                     quick(array, index, right)
                 }
             }
         }
-        
+
         var partition = function (array, left, right) {
             var pivot = array[Math.floor((left + right) / 2)],
                 i = left,
@@ -171,15 +171,15 @@ array.selectionSort()
 console.timeEnd('time')
 
 
-// 测试ES自带的sort方法
-(function testSort() {
-    var array = []
-    for (var i = 0; i < 100000; i++) {
-        array.push(Math.round(Math.random() * 100000))
-    }
-    console.time('time')
-    array.sort((a, b) => {
-        return a - b
-    })
-    console.timeEnd('time')
-}())
+    // 测试ES自带的sort方法
+    (function testSort() {
+        var array = []
+        for (var i = 0; i < 100000; i++) {
+            array.push(Math.round(Math.random() * 100000))
+        }
+        console.time('time')
+        array.sort((a, b) => {
+            return a - b // 如果小于0，a排在b之前; 等于0，a和b的相对位置不变；大于0，b排在a之前；
+        })
+        console.timeEnd('time')
+    }())
