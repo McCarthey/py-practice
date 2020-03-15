317542b684db2faa85940945a57043ca030948e8
Randall <randall@duomai.com>
2020-03-15 13:24:55 +0800

    [add] linkedList: remove duplicate node

---
+++ data-structure/linkList/remove-duplicate-node.js
@@ -0,0 +1,21 @@
+/**
+ * 面试题 02.01. 移除重复节点
+ * 编写代码，移除未排序链表中的重复节点。保留最开始出现的节点。
+ * 
+ * 示例1:
+ * 输入：[1, 2, 3, 3, 2, 1]
+ * 输出：[1, 2, 3]
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function removeDuplicateNode(head) {
+}
