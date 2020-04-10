d390e909dd28270b9823c872a1f432db05b9f14e
Randall <randall@duomai.com>
2020-04-10 15:55:15 +0800

    [add] useState with Set() Map()

--- react-learning/src/hooks/useState/useState.tsx
+++ react-learning/src/hooks/useState/useState.tsx
@@ -7,6 +7,19 @@ function App() {
     count: 0,
     name: "test"
   });
+
+  // 当使用 Set Map 时，需要根据旧值重新生成新值，操作后再设置状态
+  const [currentUAs, setCurrentUAs] = useState<Set<string>>(new Set())
+  const handleClickUA = (name: string) => {
+    if (currentUAs.has(name)) {
+      const newData = new Set(currentUAs);
+      newData.delete(name);
+      setCurrentUAs(newData);
+    } else {
+      setCurrentUAs(new Set(currentUAs).add(name))
+    }
+  }
+
   return (
     <div className="App">
       Count: {obj.count}
