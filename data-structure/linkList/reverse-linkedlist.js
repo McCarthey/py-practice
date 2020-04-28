eda2bb5284d19da2ea5ab2356de80e7a105ba24a
Randall <randall@duomai.com>
2020-04-28 19:56:32 +0800

    [add] reverse linkedList

---
+++ data-structure/linkList/reverse-linkedlist.js
@@ -0,0 +1,32 @@
+/**
+ * leetcode NO.26 反转一个单链表
+ * 示例:
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ */
+
+/**
+* Definition for singly-linked list.
+* function ListNode(val) {
+*     this.val = val;
+*     this.next = null;
+* }
+*/
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// 迭代法，保存后继结点，断开链接，链接前序结点，移动指针
+function reverseLinkedList(head) {
+  let cur = head
+  let prev = null
+  while (cur !== null) {
+    let temp = cur.next
+    cur.next = prev
+    prev = cur
+    cur = temp
+  }
+  return prev
+}
