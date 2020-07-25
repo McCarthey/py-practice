317f3106127db540c205a6817dd53e99283f9860
Randall <randall@duomai.com>
2020-07-25 13:58:22 +0800

    [add] key as index

---
+++ react-learning/diff/key.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { produce } from "immer";
+// 实验测试 索引作为 key 的弊端
+
+const TestKey = () => {
+  const [list, setList] = useState<any[]>([
+    { id: 0, name: "hi" },
+    { id: 1, name: "hi" },
+    { id: 2, name: "hi" },
+  ]);
+
+  return (
+    <>
+      {list.map((l, i) => (
+        // key 不要使用数组索引！此处可以使用 l.id
+        <div key={i}>
+          <span>{l.id}</span>
+          <input defaultValue="hi" />
+          <button
+            onClick={() => {
+              setList(
+                produce(list, (draft) => {
+                  draft.splice(i, 1);
+                })
+              );
+            }}
+          >
+            delete
+          </button>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default TestKey;
