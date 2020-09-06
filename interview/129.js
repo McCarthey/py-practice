15fa6996301226735b0c31070846d18e3d21f893
Randall <randall@duomai.com>
2020-09-06 09:15:20 +0800

    [add] console.time console.timeEnd

--- interview/129.js
+++ interview/129.js
@@ -1,48 +1,50 @@
 /**
  * 下面代码的输出
+ * （提示：console.time(timerName)是启用一个定时器用于跟踪操作所占时长，不显示指定定时器名称的话，默认default；
+ * 调用console.timeEnd()停止定时器，并以毫秒输出定时器记录的时间）
  */
 function wait() {
-    return new Promise(resolve =>
-        setTimeout(resolve, 10 * 1000)
-    )
+  return new Promise(resolve =>
+    setTimeout(resolve, 10 * 1000)
+  )
 }
 
 async function main() {
-    console.time();
-    const x = wait();
-    const y = wait();
-    const z = wait();
-    await x;
-    await y;
-    await z;
-    console.timeEnd();
+  console.time();
+  const x = wait();
+  const y = wait();
+  const z = wait();
+  await x;
+  await y;
+  await z;
+  console.timeEnd();
 }
 main();
 
 /**扩展:
  * 来自MDN(https://beta.developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Statements/async_function)
  */
-var resolveAfter2Seconds = function() {
+var resolveAfter2Seconds = function () {
   console.log("starting slow promise");
   return new Promise(resolve => {
-    setTimeout(function() {
+    setTimeout(function () {
       resolve("slow");
       console.log("slow promise is done");
     }, 2000);
   });
 };
 
-var resolveAfter1Second = function() {
+var resolveAfter1Second = function () {
   console.log("starting fast promise");
   return new Promise(resolve => {
-    setTimeout(function() {
+    setTimeout(function () {
       resolve("fast");
       console.log("fast promise is done");
     }, 1000);
   });
 };
 
-var sequentialStart = async function() {
+var sequentialStart = async function () {
   console.log('==SEQUENTIAL START==');
 
   // 1. Execution gets here almost instantly
@@ -53,7 +55,7 @@ var sequentialStart = async function() {
   console.log(fast); // 3. this runs 3 seconds after 1.
 }
 
-var concurrentStart = async function() {
+var concurrentStart = async function () {
   console.log('==CONCURRENT START with await==');
   const slow = resolveAfter2Seconds(); // starts timer immediately
   const fast = resolveAfter1Second(); // starts timer immediately
@@ -63,7 +65,7 @@ var concurrentStart = async function() {
   console.log(await fast); // 3. this runs 2 seconds after 1., immediately after 2., since fast is already resolved
 }
 
-var concurrentPromise = function() {
+var concurrentPromise = function () {
   console.log('==CONCURRENT START with Promise.all==');
   return Promise.all([resolveAfter2Seconds(), resolveAfter1Second()]).then((messages) => {
     console.log(messages[0]); // slow
@@ -71,19 +73,19 @@ var concurrentPromise = function() {
   });
 }
 
-var parallel = async function() {
+var parallel = async function () {
   console.log('==PARALLEL with await Promise.all==');
-  
+
   // Start 2 "jobs" in parallel and wait for both of them to complete
   await Promise.all([
-      (async()=>console.log(await resolveAfter2Seconds()))(),
-      (async()=>console.log(await resolveAfter1Second()))()
+    (async () => console.log(await resolveAfter2Seconds()))(),
+    (async () => console.log(await resolveAfter1Second()))()
   ]);
 }
 
 // This function does not handle errors. See warning below!
-var parallelPromise = function() {
+var parallelPromise = function () {
   console.log('==PARALLEL with Promise.then==');
-  resolveAfter2Seconds().then((message)=>console.log(message));
-  resolveAfter1Second().then((message)=>console.log(message));
+  resolveAfter2Seconds().then((message) => console.log(message));
+  resolveAfter1Second().then((message) => console.log(message));
 }
