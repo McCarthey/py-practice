096fa29579ca8319183bff18abeb8c9bcb1fa102
Randall <randall@duomai.com>
2020-09-11 11:03:14 +0800

    [add] 不使用字符串slice操作获取特定位数字

--- interview/121.js
+++ interview/121.js
@@ -19,6 +19,27 @@ function howManyOnes(n) {
     }
 }
 
+/**
+ * 不使用字符串slice操作，用取余，取商的方式获取数字
+ */
+function howManyOnesByNumber(n) {
+    if (n < 1) {
+        return 0
+    }
+    if (n < 10) {
+        return 1
+    }
+    const length = String(n).length
+    const rest = n % 10 ** (length - 1)
+    const head = Math.floor(n / 10 ** (length - 1))
+    const power = 10 ** (length - 1)
+    if (head === 1) {
+        return howManyOnes(rest) + howManyOnes(power - 1) + rest + 1
+    } else {
+        return howManyOnes(rest) + power + howManyOnes(power - 1) * head
+    }
+}
+
 console.time('count');
 console.log(howManyOnes(4925))
 console.timeEnd('count');
