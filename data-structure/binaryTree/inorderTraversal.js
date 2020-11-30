d671938523db69630a3b24b8cd7ad071c76e391b
Randall <randall@duomai.com>
2020-11-30 09:54:24 +0800

    [add] binaryTree inorder

---
+++ data-structure/binaryTree/inorderTraversal.js
@@ -0,0 +1,31 @@
+/**
+ * 给定一个二叉树，返回它的 中序 遍历。
+ * 递归版本很简单，能用迭代实现吗
+ */
+
+/**递归 */
+function inorderTraversalRecursive(root, array) {
+  if (root) {
+    inorderTraversalRecursive(root.left, array)
+    array.push(root.val)
+    inorderTraversalRecursive(root.right, array)
+  }
+  return array
+}
+
+/**迭代版本，显示维护一个栈结构 */
+function inorderTraversal(root) {
+  let current = root
+  const stack = []
+  const result = []
+  while (current || stack.length > 0) {
+    while (current) {
+      stack.push(current)
+      current = current.left
+    }
+    current = stack.pop()
+    result.push(current.val)
+    current = current.right
+  }
+  return result
+}
