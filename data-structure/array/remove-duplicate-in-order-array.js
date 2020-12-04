369d79f52634e8174ddfefca774126047c3672ae
Randall <randall@duomai.com>
2020-12-04 11:54:06 +0800

    [opt] optimize  Leetcode No.26

--- data-structure/array/remove-duplicate-in-order-array.js
+++ data-structure/array/remove-duplicate-in-order-array.js
@@ -1,4 +1,5 @@
 /**
+ * Leetcode No26. 删除排序数组中的重复项
  * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
 
   不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
@@ -11,15 +12,11 @@
   你不需要考虑数组中超出新长度后面的元素。
  */
 const removeDuplicateInOrderArray = (array) => {
-  let fast = 0
   let slow = 0
-  while (fast < array.length) {
-    if (array[slow] !== array[fast]) {
-      array[slow + 1] = array[fast]
+  for (let index = 1; index < array.length; index++) {
+    if (array[index] !== array[slow]) {
       slow++
-      fast++
-    } else {
-      fast++
+      array[slow] = array[index]
     }
   }
   return slow + 1
