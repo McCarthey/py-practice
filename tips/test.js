313fd2363ea3d21a96bf7576a85924a307c2af07
Randall <randall@duomai.com>
2020-12-22 12:20:03 +0800

    [add] test.js

---
+++ tips/test.js
@@ -0,0 +1,121 @@
+Function.prototype.myBind = function (context) {
+  const fn = this
+  const restArgs = [...arguments].slice(1)
+  return function () {
+    const args = [...restArgs, ...arguments]
+    return fn.apply(context, args)
+  }
+}
+
+Function.prototype.myCall = function (context) {
+  const ctx = context || window
+  ctx.fn = this
+  const restArgs = [...arguments].slice(1)
+  const result = ctx.fn(...restArgs)
+  delete ctx.fn
+  return result
+}
+
+Function.prototype.myApply = function (context) {
+  const ctx = context || window
+  ctx.fn = this
+  const restArgs = [...arguments][1]
+  const result = ctx.fn(...restArgs)
+  delete ctx.fn
+  return result
+}
+
+function myInstanceOf(left, right) {
+  if (typeof left !== 'object' || typeof left !== 'function' || left === null) return false
+  let proto = Object.getPrototypeOf(left)
+  while (true) {
+    if (proto === null) {
+      return false
+    }
+    if (proto === right.prototype) {
+      return true
+    }
+    proto = Object.getPrototypeOf(proto)
+  }
+}
+
+/**
+ * https://mp.weixin.qq.com/s/m3a6vjp8-c9a2EYj0cDMmg
+ * https://juejin.cn/post/6844903470466629640
+ * 注意：由于任务头不同，因此不同的任务需要放入不同的任务队列，
+ * 比如区分setTimeout/setImmediate  process.nextTick 和 promise
+ */
+
+console.log('golb1');
+
+setTimeout(function () {
+  console.log('timeout1');
+  process.nextTick(function () {
+    console.log('timeout1_nextTick');
+  })
+  new Promise(function (resolve) {
+    console.log('timeout1_promise');
+    resolve();
+  }).then(function () {
+    console.log('timeout1_then')
+  })
+})
+
+setImmediate(function () {
+  console.log('immediate1');
+  process.nextTick(function () {
+    console.log('immediate1_nextTick');
+  })
+  new Promise(function (resolve) {
+    console.log('immediate1_promise');
+    resolve();
+  }).then(function () {
+    console.log('immediate1_then')
+  })
+})
+
+process.nextTick(function () {
+  console.log('glob1_nextTick');
+})
+new Promise(function (resolve) {
+  console.log('glob1_promise');
+  resolve();
+}).then(function () {
+  console.log('glob1_then')
+})
+
+setTimeout(function () {
+  console.log('timeout2');
+  process.nextTick(function () {
+    console.log('timeout2_nextTick');
+  })
+  new Promise(function (resolve) {
+    console.log('timeout2_promise');
+    resolve();
+  }).then(function () {
+    console.log('timeout2_then')
+  })
+})
+
+process.nextTick(function () {
+  console.log('glob2_nextTick');
+})
+new Promise(function (resolve) {
+  console.log('glob2_promise');
+  resolve();
+}).then(function () {
+  console.log('glob2_then')
+})
+
+setImmediate(function () {
+  console.log('immediate2');
+  process.nextTick(function () {
+    console.log('immediate2_nextTick');
+  })
+  new Promise(function (resolve) {
+    console.log('immediate2_promise');
+    resolve();
+  }).then(function () {
+    console.log('immediate2_then')
+  })
+})
