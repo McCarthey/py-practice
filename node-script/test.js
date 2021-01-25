3c4f4ab12368f54578923d1578ffd832926ddc8f
Randall <randall@duomai.com>
2021-01-25 14:35:00 +0800

    [fix] exec stdout includes \n

--- node-script/test.js
+++ node-script/test.js
@@ -2,10 +2,23 @@ const fs = require('fs')
 const os = require('os')
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-exec('git pull', (err, stdout, stderr) => {
-  console.log('组件打包结果：', stdout)
-  if (stdout.includes('up to')) {
-    console.log('[哦吼]')
+
+
+exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
+  console.log('git 分支结果', stdout, stdout.split(''))
+  if (stdout.includes('test')) {
+    console.log('[哦吼是test分支，拉代码！]')
+    exec('git pull', (e, sto, ste) => {
+      console.log('[git pull结果]', sto)
+    })
+    return
   }
-  console.log('组件打包报错：', stderr)
 })
+
+// exec('git branch', (err, stdout, stderr) => {
+//   console.log('组件打包结果：', stdout)
+//   if (stdout.includes('test')) {
+//     console.log('[哦吼]')
+//   }
+//   console.log('组件打包报错：', stderr)
+// })
