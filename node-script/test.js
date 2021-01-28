678eece833d87a5e224fcb3e975aef481cc2347e
Randall <randall@duomai.com>
2021-01-28 11:23:28 +0800

    [add] git delete branch

--- node-script/test.js
+++ node-script/test.js
@@ -1,15 +1,18 @@
-const fs = require('fs')
-const os = require('os')
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const exec = require('child_process').exec
 
 
 exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
   console.log('git 分支结果', stdout, stdout.split(''))
   // if (stdout.includes('test')) {
-  console.log('[哦吼是test分支，拉代码！]')
-  exec('git pull', (e, sto, ste) => {
-    console.log('[git pull结果]', sto)
+  // console.log('[哦吼是test分支，拉代码！]')
+  // exec('git ci -m "[test], another commit"', (e, sto, ste) => {
+  //   console.log('[git commit报错]', e)
+  //   console.log('[git commit报错]', sto)
+  // })
+  exec('git add .', (e, sto, ste) => {
+    console.log('[git add报错]', e)
+    console.log('[git add结果]', sto)
+    console.log('[git add sterr]', ste)
   })
   return
   // }
